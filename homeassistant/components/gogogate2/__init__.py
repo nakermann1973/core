a0ff23f242b087c03e3577563c00dc5e19f863c0
Chris Picton <chris@picton.nom.za>
2024-01-08 16:01:36 +0100

    Attempt to connect during setup to ensure device is available

--- homeassistant/components/gogogate2/__init__.py
+++ homeassistant/components/gogogate2/__init__.py
@@ -1,13 +1,18 @@
 """The gogogate2 component."""
 
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_DEVICE, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
-from .common import get_data_update_coordinator
+from .common import get_api, get_data_update_coordinator
 from .const import DEVICE_TYPE_GOGOGATE2
 
 PLATFORMS = [Platform.COVER, Platform.SENSOR]
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -23,6 +28,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     if config_updates:
         hass.config_entries.async_update_entry(entry, data=config_updates)
+
+    # Make initial connection to ensure the device is available
+    api = get_api(hass, entry.data)
+    try:
+      rv = await api.async_info()
+      # TODO: check for a valid return here
+      _LOGGER.info("info returned: %s", rv)
+    except Exception as ex:
+      # TODO: Catch specific exception types here
+      _LOGGER.error("Error connecting to gogogate: %s", ex)
+      raise ConfigEntryNotReady("Connection to gogogate failed") from ex
 
     data_update_coordinator = get_data_update_coordinator(hass, entry)
     await data_update_coordinator.async_config_entry_first_refresh()
